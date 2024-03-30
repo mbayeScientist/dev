264723dee617d8317cd1d0b5f1b617316d3c2ad8
mbaye <mouhamed.mbaye@etu.univ-lyon1.fr>
2024-03-30 03:20:47 +0100

    modif

--- projet1/settings.py
+++ projet1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3v&*jf!58fisqh-m&07vkala^)ai3obi%c)rrp$77m38r9f9=u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
